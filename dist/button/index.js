e3bd26f9be5742e91960ce378c0d50490957e4d8
ayang <851051279@qq.com>
2020-03-09 14:59:13 +0800

    btn组件基本开发完成

--- dist/button/index.js
+++ dist/button/index.js
@@ -1,8 +1,30 @@
 Component({
-  data: {
-    number: 100
+  externalClasses: ['l-class'],
+  properties: {
+    // default, primary, text, success, warning, error
+    type: {
+      type: String,
+      value: ''
+    },
+    // default, large, small
+    size: {
+      type: String,
+      value: ''
+    },
+    disabled: {
+      type: Boolean,
+      value: false
+    },
+    loading: {
+      type: Boolean,
+      value: false
+    },
+    long: {
+      type: Boolean,
+      value: false
+    },
+    openType: String
   },
-  properties: {},
   methods: {
   }
 })
