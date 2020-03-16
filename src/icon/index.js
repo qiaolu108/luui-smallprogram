81ba3c1f1f28c36285d2cd021ae591cd6a72c169
ayang <851051279@qq.com>
2020-03-16 16:08:04 +0800

    修改icon组件

--- src/icon/index.js
+++ src/icon/index.js
@@ -1,11 +1,6 @@
 Component({
   externalClasses: ['l-class'],
   properties: {
-    // 原生icon 的type 类型，可以使用小程序原生自带的图表
-    type: {
-      type: String,
-      value: ''
-    },
     // 图标大小
     size: {
       type: Number,
